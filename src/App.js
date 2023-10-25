03adf1f4e39e880035a14afdc34506be31fff755
Franz <franzmarte23@gmail.com>
2023-10-25 15:16:12 -0700

    adds winner notfication

--- src/App.js
+++ src/App.js
@@ -8,13 +8,35 @@ const App = () => {
   const [next, setNext] = useState(false)
   console.log(squares)
 
+  function calculateWinner(squares) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          alert(`${squares[a]}  is the winner`)
+          return squares[a];
+        }
+      }
+      return null;
+    }
+
+
   const handleClick= (i) => {
     if (squares[i] === null) {
       if (next === false) {
         squares[i] = "x"
       } else if (next === true) {
         squares[i] = "o"
-      }
+      } 
     }
     
     
@@ -22,7 +44,29 @@ const App = () => {
     console.log(squares[i])
     setNext(prev => !prev)
     console.log(next)
+
+
+    calculateWinner(squares)
   }
+  // function calculateWinner(squares) {
+  //   const lines = [
+  //     [0, 1, 2],
+  //     [3, 4, 5],
+  //     [6, 7, 8],
+  //     [0, 3, 6],
+  //     [1, 4, 7],
+  //     [2, 5, 8],
+  //     [0, 4, 8],
+  //     [2, 4, 6],
+  //   ];
+  //   for (let i = 0; i < lines.length; i++) {
+  //     const [a, b, c] = lines[i];
+  //     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+  //       return squares[a];
+  //     }
+  //   }
+  //   return null;
+  // }
 
   return (
     <>
